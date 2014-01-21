d396bb8a1c6bc896b859c3866dbee3aa4a0d55db
Alan Said <alan@alans.se>
2014-01-21 18:33:07 +0100

    Filter out non-us users without state

--- R/analyze.R
+++ R/analyze.R
@@ -1,4 +1,5 @@
 print("Loading...")
+#users <- read.table('../parsed/1kusers.tsv', header=TRUE, sep="\t", quote="", comment.char="")
 users <- read.table('../parsed/parsedProfiles.tsv', header=TRUE, sep="\t", quote="", comment.char="")
 ratings <- read.table('../parsed/parsedRatings.tsv', header=TRUE, sep="\t", quote="", comment.char="")
 recipes <- read.table('../parsed/parsedRecipes.tsv', header=TRUE, sep="\t", quote="", comment.char="")
@@ -8,12 +9,18 @@ ingredients <- read.table('../parsed/parsedIngredients.tsv', header=TRUE, sep="\
 #interests <- read.table('../parsed/interest.tsv', header=TRUE, sep="\t", quote="", comment.char="")
 print("Finished loading")
 
+
+print("Filtering out non-US users and users without a state")
+ususers = users[ which(users$livingcountry == " USA" & users$livingstate != ""),]
+
+
+
 print("Merging recipes + ingredients")
 recipes_ing <- merge(recipes, ingredients, by=c("recID"))
 
 print("Merging users and ratings")
 #users.filtered <- users[users$livingcountry == " USA"]
-user_ratings <- merge(users, ratings, by=c("uid"))
+user_ratings <- merge(ususers, ratings, by=c("uid"))
 
 # never do this, 20+gb RAM
 #all_merged <- merge(recipes_ing, user_ratings, by.x="recID", by.y="recid")
@@ -21,9 +28,10 @@ user_ratings <- merge(users, ratings, by=c("uid"))
 N=10
 
 print("Merging per state")
-states <- unique(users$livingstate)
+states <- unique(ususers$livingstate)
 for (state in states){
 	if(state == ""){
+		print("should not happen")
 		next
 	}
 	print(paste("Merging user and ratings for state ", state, sep=""))
