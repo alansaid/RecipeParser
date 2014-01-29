0949bacff3f5a1860729f1c744e4697d50e02418
alansaid <alan@alans.se>
2014-01-29 18:39:03 +0100

    analyzeByTown done

--- R/analyzeByTown.R
+++ R/analyzeByTown.R
@@ -1,6 +1,6 @@
 print("Loading...")
-users <- read.table('../parsed/1kusers.tsv', header=TRUE, sep="\t", quote="", comment.char="")
-#users <- read.table('../parsed/parsedProfiles.tsv', header=TRUE, sep="\t", quote="", comment.char="")
+#users <- read.table('../parsed/100users.tsv', header=TRUE, sep="\t", quote="", comment.char="")
+users <- read.table('../parsed/parsedProfiles.tsv', header=TRUE, sep="\t", quote="", comment.char="")
 ratings <- read.table('../parsed/parsedRatings.tsv', header=TRUE, sep="\t", quote="", comment.char="")
 recipes <- read.table('../parsed/parsedRecipes.tsv', header=TRUE, sep="\t", quote="", comment.char="")
 ingredients <- read.table('../parsed/parsedIngredients.tsv', header=TRUE, sep="\t", quote="", comment.char="")
@@ -26,7 +26,7 @@ user_ratings <- merge(ususers, ratings, by=c("uid"))
 #all_merged <- merge(recipes_ing, user_ratings, by.x="recID", by.y="recid")
 
 N=100
-cat("State\tTown\tingrID\tcount")
+cat("State\tTown\tingrID\trecipes\tcount", file="townCounts.tsv", append=TRUE)
 
 print("Merging per state")
 states <- unique(ususers$livingstate)
@@ -47,30 +47,16 @@ for (state in states){
 		#merge ratings with ingredients, by town in state
 		town.full <- merge(recipes_ing, user_ratings.town, by.x="recID", by.y="recid")
 		town.state.df <- data.frame(s=town.full$siteID, recID=town.full$recID)
+		town.state.recipes <- unique(town.state.df$recID)
 		town.state.all.agg <- aggregate(recID ~ s, data=town.state.df, FUN=length)
 		town.state.sorted.indices <- order(town.state.all.agg$recID, decreasing=T)
 		counts <- town.state.all.agg$recID[town.state.sorted.indices][1:N]
 		ingredients <- town.state.all.agg$s[town.state.sorted.indices][1:N]
 		for (i in town.state.sorted.indices[1:N]){
-			cat(state, town, town.state.all.agg$s[i], town.state.all.agg$recID[i], file="townCounts.tsv", append=TRUE, '\n', sep="\t")
+			if(is.na(town.state.all.agg$recID[i]) || is.na(town.state.all.agg$s[i])){
+				next
+			}
+			cat(state, town, town.state.all.agg$s[i], town.state.all.agg$recID[i], length(town.state.recipes), file="townCounts.tsv", append=TRUE, '\n', sep="\t")
 		} 
-#		break
 	}
-#	break
-
-
-	# merge with recipes and the smaller/filtered states table
-#	state.all <- merge(recipes_ing, user_ratings.state, by.x="recID", by.y="recid")
-
-	#print(head(state.all, 1))
-	# small data frame with only two columns: ingredient id (siteID) and another column (irrelevant)
-#	state.all.df <- data.frame(s=state.all$siteID, r=state.all$recID)
-#	state.all.agg <- aggregate(r ~ s, data=state.all.df, FUN=length)
-#	sorted.indices <- order(state.all.agg$r, decreasing=T)
-#	counts <- state.all.agg$r[sorted.indices][1:N]
-#	ingredients <- state.all.agg$s[sorted.indices][1:N]
-#	for (i in sorted.indices[1:N]){
-#		cat(state, state.all.agg$s[i], state.all.agg$r[i], '\n', sep="\t")
-#	}
-#	break
 }
